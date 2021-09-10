50c707276072f883994f88cbdf23867afa24ea12
uoa-pgul065 <pgul065@aucklanduni.ac.nz>
2021-09-10 21:09:29 +1200

    Worked on Fundamentals Upto Video 10

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,28 @@
+// Pranav Gulati
+
+// As using multiple scripts in same html file
+// Adding Divider
+console.log("****** Assignment File ******");
+
+
+
+// L1 Variable
+
+const country = "India";
+const continent = "Asia";
+let population = "136"; // In millions
+
+console.log("Country is: " + country);
+console.log("Continent is: " + continent);
+console.log("Population (millions) is: " + population);
+
+// L2 Data Types
+const isIsland = false;
+let language;
+
+console.log("isIsland: " + isIsland);
+
+// L3 let, const and var
+language = "English";
+// isIsland = true; - Error: Assignment to const value.
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+// VIDEO 9
+// let js = "Gulati Saab";
+// if (js == "Gulati Saab") {
+//     alert("Gulati Saab is Here!");
+// }
+
+// console.log(40 + 8 + 23 - 12);
+
+// VIDEO 10
+let js = "Gulati Saab";
+
+console.log(40 + 8 + 23 - 12);
+
+console.log("Pranav");
+console.log(20);
+
+// Declaring Variable
+let firstName = "Pranav";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
